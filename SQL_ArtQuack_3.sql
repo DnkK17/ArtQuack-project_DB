e5fc85eaeb6508c2033936e043e3829633d313ac
DnkK17 <89891970+DnkK17@users.noreply.github.com>
2023-10-14 18:47:18 +0700

    Update SQL_ArtQuack_3.sql - int to varchar (primary key)
    Except Level and Category

--- SQL_ArtQuack_3.sql
+++ SQL_ArtQuack_3.sql
@@ -9,13 +9,13 @@ CREATE TABLE [Role] (
 );
 
 CREATE TABLE [UserRole] (
-  userRoleID int PRIMARY KEY,
+  userRoleID varchar PRIMARY KEY,
   roleID int FOREIGN KEY REFERENCES [Role]([roleID])
 );
 
 CREATE TABLE [Instructor] (
-  instructorID int PRIMARY KEY,
-  userRoleID int FOREIGN KEY REFERENCES [UserRole]([userRoleID]),
+  instructorID varchar PRIMARY KEY,
+  userRoleID varchar FOREIGN KEY REFERENCES [UserRole]([userRoleID]),
   [name] varchar(30),
   email varchar(30),
   [certificate] varchar(500),
@@ -25,17 +25,17 @@ CREATE TABLE [Instructor] (
 );
 
 CREATE TABLE [Category] (
-  cateID int PRIMARY KEY,
+  cateID varchar PRIMARY KEY,
   [name] varchar(500)
 );
 
 CREATE TABLE [Course] (
   [course_code] varchar(30),
-  instructorID int FOREIGN KEY REFERENCES [Instructor]([instructorID]),
-  cateID int FOREIGN KEY REFERENCES [Category]([cateID]),
+  instructorID varchar FOREIGN KEY REFERENCES [Instructor]([instructorID]),
+  cateID varchar FOREIGN KEY REFERENCES [Category]([cateID]),
   levelID int FOREIGN KEY REFERENCES [Level]([levelID]),
-  reviewID int,
-  courseID int PRIMARY KEY,
+  reviewID varchar,
+  courseID varchar PRIMARY KEY,
   [name] varchar(500),
   [description] varchar(500),
   [upload_date] date,
@@ -46,16 +46,16 @@ CREATE TABLE [Course] (
 );
 
 CREATE TABLE [Topic] (
-  topicID int PRIMARY KEY,
+  topicID varchar PRIMARY KEY,
   [name] varchar(500),
   videoURL varchar(1000),
-  courseID int FOREIGN KEY REFERENCES [Course]([courseID])
+  courseID varchar FOREIGN KEY REFERENCES [Course]([courseID])
 );
 
 CREATE TABLE [User] (
   userName varchar(100) PRIMARY KEY,
-  userRoleID int FOREIGN KEY REFERENCES [UserRole]([userRoleID]),
-  id int,
+  userRoleID varchar FOREIGN KEY REFERENCES [UserRole]([userRoleID]),
+  id varchar,
   [name] varchar(100),
   email varchar(500),
   [password] varchar(30),
@@ -63,33 +63,33 @@ CREATE TABLE [User] (
 );
 
 CREATE TABLE [Enroll] (
-  enrollID int PRIMARY KEY,
+  enrollID varchar PRIMARY KEY,
   userName varchar(100) FOREIGN KEY REFERENCES [User]([userName]),
-  [courseID] int FOREIGN KEY REFERENCES [Course]([courseID]),
+  [courseID] varchar FOREIGN KEY REFERENCES [Course]([courseID]),
   [status] bit
 );
 
 CREATE TABLE [EnrollTopic] (
-  enTopicID int PRIMARY KEY,
-  topicID int FOREIGN KEY REFERENCES [Topic]([topicID]),
-  enrollID int FOREIGN KEY REFERENCES [Enroll]([enrollID]),
+  enTopicID varchar PRIMARY KEY,
+  topicID varchar FOREIGN KEY REFERENCES [Topic]([topicID]),
+  enrollID varchar FOREIGN KEY REFERENCES [Enroll]([enrollID]),
   enroll_date date,
   [status] bit
 );
 
 CREATE TABLE [ArtWork] (
-  artworkID int PRIMARY KEY,
-  enTopicID int FOREIGN KEY REFERENCES [EnrollTopic]([enTopicID]),
-  userID int,
-  instructorID int,
+  artworkID varchar PRIMARY KEY,
+  enTopicID varchar FOREIGN KEY REFERENCES [EnrollTopic]([enTopicID]),
+  userID varchar,
+  instructorID varchar,
   [status] bit,
 );
 
 CREATE TABLE [Review] (
-  reviewID int PRIMARY KEY,
-  artworkID int FOREIGN KEY REFERENCES [ArtWork]([artworkID]),
-  userID int,
-  instructorID int,
+  reviewID varchar PRIMARY KEY,
+  artworkID varchar FOREIGN KEY REFERENCES [ArtWork]([artworkID]),
+  userID varchar,
+  instructorID varchar,
   rate int,
   comment varchar(500),
   [date] date,
@@ -97,10 +97,10 @@ CREATE TABLE [Review] (
 );
 
 CREATE TABLE [Post] (
-  postID int PRIMARY KEY,
-  userRoleID int FOREIGN KEY REFERENCES [UserRole]([userRoleID]), //empty userRoleID - replace w 'title'
-  cateID int FOREIGN KEY REFERENCES [Category]([cateID]),
-  topicID int FOREIGN KEY REFERENCES [Topic]([topicID]),
+  postID varchar PRIMARY KEY,
+  title varchar (300)
+  cateID varchar FOREIGN KEY REFERENCES [Category]([cateID]),
+  topicID varchar FOREIGN KEY REFERENCES [Topic]([topicID]),
   content varchar(3000),
   [status] bit,
 );
